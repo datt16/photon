7135466fb4d3993cd59895a317ca064c5bcedc40
datt16 <osatom.4639@gmail.com>
2022-09-03 01:07:08 +0900

    hook更新, フォーマットのみへ

--- .lintstagedrc.js
+++ .lintstagedrc.js
@@ -1,9 +1,6 @@
 // .lintstagedrc.js
 module.exports = {
     'pages/*.{js,jsx,ts,tsx}': [
-      (filenames) =>
-        `next lint --fix --file ${filenames
-          .map((file) => file.split(process.cwd())[1])}`,
       'prettier --write',
     ],
   }
