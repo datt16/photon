d2576515207d800b3091c1f73b413c029e678d06
datt16 <osatom.4639@gmail.com>
2022-09-03 01:01:22 +0900

    chakraUI追加

--- pages/babylon/UserScene.tsx
+++ pages/babylon/UserScene.tsx
@@ -1,15 +1,20 @@
+import { Switch } from "@chakra-ui/react"
 import {
   ArcRotateCamera,
+  BoxParticleEmitter,
   DirectionalLight,
+  Mesh,
   MeshBuilder,
   PointLight,
   Scene,
   Vector3,
 } from "babylonjs"
-import React, { Ref } from "react"
+import React, { Ref, useState } from "react"
 import FakeScene from "./FakeScene"
 
 const UserScene = () => {
+  const [SelectedItem, setSelectedItem] = useState<Mesh | null>(null)
+  const [selectedOption, setSelectedOption] = useState<any>()
   const onSceneReady = (scene: Scene, canvas: Ref<HTMLCanvasElement>) => {
     const camera = new ArcRotateCamera(
       "camera",
@@ -31,12 +36,28 @@ const UserScene = () => {
     new DirectionalLight("PointLight", new Vector3(0, -1, 0), scene)
     camera.setTarget(box.position)
     camera.attachControl(canvas, true)
+
+    setSelectedItem(box)
   }
 
   return (
     <div>
       <p>FakeScene</p>
-      <button>Hello</button>
+      <Switch
+        onChange={() => {
+          if (SelectedItem !== null) {
+            if (selectedOption) {
+              SelectedItem.scaling = new Vector3(1, 1, 1)
+              setSelectedOption(false)
+            } else {
+              SelectedItem.scaling = new Vector3(2, 2, 2)
+              setSelectedOption(true)
+            }
+          }
+        }}
+      >
+        isBig?
+      </Switch>
       <FakeScene onRender={() => {}} onSceneReady={onSceneReady} />
     </div>
   )
