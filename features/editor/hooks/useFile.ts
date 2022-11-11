5c6f1a0acce79db2cded1d285e8f4c2438cb8926
datt16 <osatom.4639@gmail.com>
2022-11-11 17:23:11 +0900

    useFileフックを更新
    バックエンドと通信するようにした

--- features/editor/hooks/useFile.ts
+++ features/editor/hooks/useFile.ts
@@ -1,15 +1,35 @@
+import axios, { AxiosRequestConfig } from "axios"
 import { ChangeEventHandler, useState } from "react"
+import { UPLOAD_FILE_FORM_FIELD_NAME } from "../../../const/const"
 
 const useFile = () => {
   const [fileURL, setFileURL] = useState<string>()
   const [fileName, setFileName] = useState<string>()
 
-  const handleFiles: ChangeEventHandler<HTMLInputElement> = (event) => {
-    const files = event.currentTarget.files
-    if (!files || files?.length === 0) return
-    const url = window.URL.createObjectURL(files[0])
-    setFileURL(url)
-    setFileName(files[0].name)
+  const handleFiles: ChangeEventHandler<HTMLInputElement> = async (event) => {
+    const config: AxiosRequestConfig = {
+      headers: { "Content-Type": "multipart/form-data" },
+      onUploadProgress: (event) => {
+        console.log(
+          `Current progress:`,
+          Math.round((event.loaded * 100) / event.total!)
+        )
+      },
+    }
+
+    if (!event.target.files?.length) {
+      return
+    }
+    const formData: FormData = new FormData()
+    Array.from(event.target.files).forEach((file, index) => {
+      setFileName(file.name)
+      formData.append(UPLOAD_FILE_FORM_FIELD_NAME, file)
+    })
+
+    const response = await axios.post("/api/uploads", formData, config)
+    if (response.status == 200) {
+      setFileURL(`/public/uploads/${fileName}`)
+    }
   }
   return { handleFiles, fileURL, fileName }
 }
