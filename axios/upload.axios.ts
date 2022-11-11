58cb1ea27b861fb69e9857dd6fa067eaa1632e76
datt16 <osatom.4639@gmail.com>
2022-11-11 10:26:36 +0900

    バックエンド作ってみた

---
+++ axios/upload.axios.ts
@@ -0,0 +1,9 @@
+export default {}
+// export const config = {
+//     headers: { 'content-type': 'multipart/form-data' },
+//     onUploadProgress: (event) => {
+//         console.log(`Current progress:`, Math.round((event.loaded * 100) / event.total));
+//     },
+// };
+
+// export const response = await axios.post('/api/uploads', formData, config);
