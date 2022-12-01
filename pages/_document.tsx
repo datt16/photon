1a9a12165a5b351d88b3797d24a20280a98ccdd8
datt16 <osatom.4639@gmail.com>
2022-12-01 16:38:44 +0900

    PWA対応

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -4,7 +4,11 @@ import { Html, Head, Main, NextScript } from "next/document"
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
+      <Head>
+        <link rel="manifest" href="/manifest.json" />
+        <link rel="apple-touch-icon" href="/icon-192x192.png" />
+        <meta name="theme-color" content="#33334c" />
+      </Head>
       <body>
         {/* 👇 Here's the script */}
         <ColorModeScript initialColorMode={"dark"} />
