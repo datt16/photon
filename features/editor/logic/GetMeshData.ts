93f03d5c08a896a5387af14bc0cc1f0b8e3a498a
datt16 <osatom.4639@gmail.com>
2022-12-12 10:29:49 +0900

    GRID, GIZMO属性の追加

--- features/editor/logic/GetMeshData.ts
+++ features/editor/logic/GetMeshData.ts
@@ -6,9 +6,11 @@ import {
   Node,
 
 } from "@babylonjs/core"
-import { MeshDataItem, SceneMeshData } from "photon-babylon"
+import { MeshDataItem, MeshDataItemType, SceneMeshData } from "photon-babylon"
+import { photonConst } from "../../../const/const"
 
 let nodes: SceneMeshData = {}
+
 const getMeshData = (rootNodes: Node[]): {} => {
   let childNode: MeshDataItem[] = []
 
@@ -17,20 +19,24 @@ const getMeshData = (rootNodes: Node[]): {} => {
     const key = item.parent?.name ? item.parent?.name : "__root__"
 
     if (child[0] == undefined) {
-      childNode.push({
-        name: item.name,
-        id: item.uniqueId.toString(),
-        type:
-          item instanceof LinesMesh
-            ? "LINE_MESH"
-            : item instanceof Camera
-            ? "CAMERA"
-            : item instanceof Light
-            ? "LIGHT"
-            : item instanceof Mesh
-            ? "MESH"
-            : "NONE",
-      })
+
+      childNode.push(
+        MeshDataFactory(
+          item.name,
+          item.uniqueId.toString(),
+          item.name.includes(photonConst.Prefix.PREFIX_3DUI_GIZMO) ? "GIZMOS" :
+            item.name.includes(photonConst.Prefix.PREFIX_3DUI_GRID) ? "GRID" :
+              item instanceof LinesMesh
+                ? "LINE_MESH"
+                : item instanceof Camera
+                  ? "CAMERA"
+                  : item instanceof Light
+                    ? "LIGHT"
+                    : item instanceof Mesh
+                      ? "MESH"
+                      : "NONE"
+        )
+      )
       nodes[key] = {
         id: key,
         child: childNode,
@@ -45,6 +51,22 @@ const getMeshData = (rootNodes: Node[]): {} => {
   })
 
   return nodes
+}
+
+const MeshDataFactory = (
+  name: string,
+  id: string,
+  type: MeshDataItemType,
+  isInspectorVisible?: boolean
+): MeshDataItem => {
+
+  return {
+    name,
+    id,
+    type,
+    isInspectorVisible: isInspectorVisible ? isInspectorVisible : !["GIZMOS", "GRID"].includes(type)
+  }
+
 }
 
 export default getMeshData
