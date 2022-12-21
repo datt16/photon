bf1720140623856c5e1e3b9846b3b18d3d7cf033
datt16 <osatom.4639@gmail.com>
2022-12-21 09:53:36 +0900

    パネルを右寄せ設定可能にした

--- src/features/editor/components/elements/panel/FloatingControlPanel.tsx
+++ src/features/editor/components/elements/panel/FloatingControlPanel.tsx
@@ -2,20 +2,24 @@ import { Box, ScaleFade } from "@chakra-ui/react"
 import { ReactNode, useState } from "react"
 const FloatingControlPanel = ({
   children,
+  position = "left",
 }: {
   children: ReactNode[] | ReactNode
+  position?: "left" | "right"
 }) => {
   const [isPanelOpen] = useState(true)
 
   return (
     <ScaleFade initialScale={0.9} in={isPanelOpen}>
       <Box
-        mt={"1rem"}
-        ml={"1rem"}
+        my={"1rem"}
+        mx={"1rem"}
         alignItems={"start"}
         display="flex"
         borderRadius="lg"
         position="fixed"
+        right={position == "right" ? 0 : "auto"}
+        left={position == "left" ? 0 : "auto"}
         zIndex={100}
         shadow="md"
         rounded="md"
